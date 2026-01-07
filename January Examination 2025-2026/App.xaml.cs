6b6347f9e418c689da90d14da31f7d4dda1efc53
MehmetBuk-STUDENT <MehmetBuk-STUDENT@S-2K1H504-SL15>
2026-01-07 09:55:46 +0000

    Add project files.

---
+++ January Examination 2025-2026/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace January_Examination_2025_2026
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ January Examination 2025-2026/Robot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace January_Examination_2025_2026
+{
+    internal class Robot
+    {
+        public string robotName { get; set; }
+
+        public double powerCapacityKWH { get; set; }
+
+       public double currentPowerKWH { get; set; }
+    }
+}
